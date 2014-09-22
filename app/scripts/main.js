38c0e802238d5519542ad4e5d4c56b7cf8047647
Drew Botka <hellodrewbot@gmail.com>
2014-09-22 14:29:29 -0400

    added icons and more styling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,19 +4,27 @@ $('.display-share-button').click(function(){
 })
 
 $('#profile-send-link button').click(function() {
-	var profileRecipient = $('#profile-recipient').val();
-	var profileSubject = $('#profile-subject').val();
-	var profileMessage = $('#profile-message').val();
-    $("#profile-send-link").attr('href',"mailto:" + profileRecipient + "?subject=" + profileSubject + "&body=" + profileMessage);
-    $('#profile-recipient').val('');
+	if (!$('#profile-recipient').val()) {
+		alert('You must enter a recipient\'s email address to send')
+	} else {
+		var profileRecipient = $('#profile-recipient').val();
+		var profileSubject = $('#profile-subject').val();
+		var profileMessage = $('#profile-message').val();
+	    $("#profile-send-link").attr('href',"mailto:" + profileRecipient + "?subject=" + profileSubject + "&body=" + profileMessage);
+	    $('#profile-recipient').val('');
+	}
 })
 
 $('#project-send-link button').click(function() {
-	var projectRecipient = $('#project-recipient').val();
-	var projectSubject = $('#project-subject').val();
-	var projectMessage = $('#project-message').val();
-    $("#project-send-link").attr('href',"mailto:" + projectRecipient + "?subject=" + projectSubject + "&body=" + projectMessage);
-    $('#project-recipient').val('');
+	if (!$('#project-recipient').val()) {
+		alert('You must enter a recipient\'s email address to send')
+	} else {
+		var projectRecipient = $('#project-recipient').val();
+		var projectSubject = $('#project-subject').val();
+		var projectMessage = $('#project-message').val();
+	    $("#project-send-link").attr('href',"mailto:" + projectRecipient + "?subject=" + projectSubject + "&body=" + projectMessage);
+	    $('#project-recipient').val('');
+	}
 })
 
 $('.submit-form').click(function(){
